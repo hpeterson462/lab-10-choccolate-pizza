f09d79f0dda60fcd5e495a6d458c17e0853b61a7
Heather Peterson <hpeterson462@gmail.com>
2020-07-23 13:52:20 -0700

    test pass

--- test/example.test.js
+++ test/example.test.js
@@ -7,10 +7,10 @@ test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = true;
 
     //Expect
     // Make assertions about what is expected versus the actual result
